df117679e665afda4453a4a7b90ddf33b4d42f24
weigego <weic6@outlook.com>
2024-07-26 02:43:42 -0500

    update db

--- crawl.py
+++ crawl.py
@@ -2,47 +2,49 @@ import re
 import requests
 import base64
 import os
+import fnmatch
+
+
+patterns_excluded = [
+    "LICENSE",
+    ".*",  # files starting with a dot
+    "_*",  # files starting with an underscore
+    "*.exe",
+    "*.dll",
+    "*.so",
+    "*.o",
+    "*.a",
+    "*.dylib",
+    "*.class",
+    "*.jar",
+    "*.war",
+    "*.zip",
+    "*.log",
+    "*.png",
+    "*.jpg",
+    "*.jpeg",
+    "*.gif",
+    "*.svg",
+    "*.ico",
+]
+
+
+def is_excluded(name):
+    return any(fnmatch.fnmatch(name, pattern) for pattern in patterns_excluded)
 
 
 def crawl_local_repo(repo_path, rel_path=False):
     """
-    return paths of all files in a local repository.
-    if rel_path is True, return relative paths. Otherwise, return absolute paths.
+    return absolute paths of all files in a local repository.
+    if rel_path is True, return relative paths.
 
     """
     all_files = []
     for root, dirs, files in os.walk(repo_path):
-        dirs[:] = [d for d in dirs if not d.startswith(".") and not d.startswith("_")]
+        dirs[:] = [d for d in dirs if not is_excluded(d)]
         for file in files:
-            if (
-                file.startswith(".")
-                or file.startswith("_")
-                or file in ["LICENSE", "package-lock.json"]
-            ):
+            if is_excluded(file):
                 continue
-            if any(
-                file.endswith(ext)
-                for ext in [
-                    ".exe",
-                    ".dll",
-                    ".so",
-                    ".o",
-                    ".a",
-                    ".dylib",
-                    ".class",
-                    ".jar",
-                    ".war",
-                    ".log",
-                    ".png",
-                    ".jpg",
-                    ".jpeg",
-                    ".gif",
-                    ".svg",
-                    ".ico",
-                ]
-            ):
-                continue
-
             path = os.path.join(root, file)
             if rel_path:
                 path = path.replace(repo_path, "")
@@ -52,47 +54,14 @@ def crawl_local_repo(repo_path, rel_path=False):
 
 def get_repo_filetype(repo_path):
     filetypes = set()
-    for root, dirs, files in os.walk(repo_path):
-        dirs[:] = [d for d in dirs if not d.startswith(".") and not d.startswith("_")]
-        for file in files:
-            if (
-                file.startswith(".")
-                or file.startswith("_")
-                or file
-                in [
-                    "LICENSE",
-                ]
-            ):
-                continue
-            if any(
-                file.endswith(ext)
-                for ext in [
-                    ".exe",
-                    ".dll",
-                    ".so",
-                    ".o",
-                    ".a",
-                    ".dylib",
-                    ".class",
-                    ".jar",
-                    ".war",
-                    ".log",
-                    ".png",
-                    ".jpg",
-                    ".jpeg",
-                    ".gif",
-                    ".svg",
-                    ".ico",
-                ]
-            ):
-                continue
-
-            _, ext = os.path.splitext(file)
-            filetypes.add(ext)
+    files = crawl_local_repo(repo_path)
+    for file in files:
+        _, ext = os.path.splitext(file)
+        filetypes.add(ext)
     return filetypes
 
 
-def extract_owner_repo(url):
+def git_extract_owner_repo(url):
     pattern = r"github\.com/([^/]+)/([^/]+)"
     match = re.search(pattern, url)
     if match:
@@ -103,7 +72,7 @@ def extract_owner_repo(url):
     return None, None
 
 
-def get_repo_contents(owner, repo, path="", github_token=None):
+def git_get_repo_contents(owner, repo, path="", github_token=None):
     url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
     headers = {"Authorization": f"token {github_token}"} if github_token else {}
     response = requests.get(url, headers=headers)
@@ -111,51 +80,11 @@ def get_repo_contents(owner, repo, path="", github_token=None):
     return response.json()
 
 
-def crawl_local_repo(repo_path, rel_path=False):
-    all_files = []
-    for root, dirs, files in os.walk(repo_path):
-        # print(root, dirs, files)
-        # Filter out directories starting with '.' and filter out "pycache"
-        dirs[:] = [d for d in dirs if not d.startswith(".") and not d.startswith("_")]
-
-        for file in files:
-            if file.startswith(".") or file.startswith("_") or file in ["LICENSE"]:
-                continue
-            if any(
-                file.endswith(ext)
-                for ext in [
-                    ".exe",
-                    ".dll",
-                    ".so",
-                    ".o",
-                    ".a",
-                    ".dylib",
-                    ".class",
-                    ".jar",
-                    ".war",
-                    ".log",
-                    ".png",
-                    ".jpg",
-                    ".jpeg",
-                    ".gif",
-                    ".svg",
-                    ".ico",
-                ]
-            ):
-                continue
-            path = os.path.join(root, file)
-            if rel_path:
-                path = path.replace(repo_path, "")
-            all_files.append(path)
-
-    return all_files
-
-
-def crawl_repo(owner, repo, github_token=None):
+def git_crawl_repo(owner, repo, github_token=None):
     all_files = []
 
     def crawl_path(path):
-        contents = get_repo_contents(owner, repo, path, github_token)
+        contents = git_get_file_content(owner, repo, path, github_token)
         for item in contents:
             if (
                 item["name"].startswith(".")
@@ -174,8 +103,8 @@ def crawl_repo(owner, repo, github_token=None):
     return all_files
 
 
-def fetch_file_content(file_path: str):
-    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{file_path}"
+def git_get_file_content(owner, repo, path, github_token)
+    url = f"https://api.github.com/repos/{owner}/{repo}/contents/{path}"
     headers = {"Authorization": f"token {github_token}"} if github_token else {}
     response = requests.get(url, headers=headers)
     response.raise_for_status()
@@ -184,13 +113,5 @@ def fetch_file_content(file_path: str):
     return base64.b64decode(content).decode("utf-8")
 
 
-def read_file_content(file_path):
-    def read_file_content(path):
-        with open(path, "rb") as file:
-            raw_data = file.read()
-            result = chardet.detect(raw_data)
-            encoding = result["encoding"]
 
-        with open(path, "r", encoding=encoding) as file:
-            content = file.read()
-        return content
+
--- db.py
+++ db.py
@@ -5,26 +5,29 @@ import sys
 
 sys.modules["sqlite3"] = sys.modules["pysqlite3"]
 
-from langchain_openai import OpenAIEmbeddings
 import os
 import shutil
 from crawl import *
 from langchain_community.vectorstores.chroma import Chroma
+from langchain.document_loaders import NotebookLoader, TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain_openai import OpenAIEmbeddings
 
 
-def get_vectordb(openai_api_key, repo_path, persist_directory=None):
-    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
-
+def get_vectordb_v3(openai_api_key, repo_path, persist_directory=None):
+    """
+    docs contain meta data
+    """
     if persist_directory is None:
         persist_directory = os.path.join(repo_path, "_chroma")
 
     if os.path.exists(persist_directory):
         shutil.rmtree(persist_directory)  # remove the directory and its contents
 
+    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+
     file_paths = crawl_local_repo(repo_path)
-
-    from langchain.docstore.document import Document
-
     documents = [
         Document(
             page_content=read_file_content(path),
@@ -43,6 +46,27 @@ def get_vectordb(openai_api_key, repo_path, persist_directory=None):
     return vectordb
 
 
+def get_vectordb(openai_api_key, repo_path, persist_directory=None):
+    if persist_directory is None:
+        persist_directory = os.path.join(repo_path, "_chroma")
+
+    if os.path.exists(persist_directory):
+        shutil.rmtree(persist_directory)  # remove the directory and its contents
+
+    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    docs = load_docs_in_repo(repo_path)
+    split_docs = split_docs(docs)  # ???: can GPT know file source?
+
+    vectordb = Chroma.from_documents(
+        documents=split_docs,
+        embedding=embedding,
+        persist_directory=persist_directory,
+    )
+    vectordb.persist()
+
+    return vectordb
+
+
 def load_vector_db(openai_api_key, persist_directory=None):
     if not os.path.exists(persist_directory):
         print(f"Persist directory {persist_directory} does not exist.")
@@ -54,3 +78,32 @@ def load_vector_db(openai_api_key, persist_directory=None):
         embedding_function=embedding,
     )
     return vectordb
+
+
+def split_docs(docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+    docs_split = text_splitter.split_documents(docs)
+    return docs_split
+
+
+def load_docs_in_repo(repo_path):
+    """
+    TODO: add more specific loaders?
+    """
+    docs = []
+    files = crawl_local_repo(repo_path)
+    for file in files:
+        try:
+            if file.endswith(".ipynb"):
+                loader = NotebookLoader(file)
+            else:
+                loader = TextLoader(file, encoding="utf-8")
+                docs.extend(loader.load_and_split())
+        except Exception as e:
+            pass
+
+    return docs
+
+
+def read_file_content(path):
+    pass
